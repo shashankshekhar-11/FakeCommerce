54f7e0403d0ad73c9e75bbc7bb21ffd49bffd3ab
Shashank <shashankshekhar746@gmail.com>
2026-04-16 01:26:03 +0530

    Added basic order APIs

--- src/main/java/com/example/FakeCommerce/controllers/OrderController.java
+++ src/main/java/com/example/FakeCommerce/controllers/OrderController.java
@@ -2,6 +2,8 @@ package com.example.FakeCommerce.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.FakeCommerce.dtos.GetOrderResponseDto;
 import com.example.FakeCommerce.schema.Order;
+import com.example.FakeCommerce.services.OrderService;
+import com.example.FakeCommerce.utils.ApiResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,9 +24,12 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
 public class OrderController {
+    private final OrderService orderService;
     @GetMapping
-    public List<Order> getAllOrders(){
-        throw new UnsupportedOperationException("Not implemented");
+    public ResponseEntity<ApiResponse<List<GetOrderResponseDto>>> getAllOrders(){
+        List<GetOrderResponseDto> orders = orderService.getAllOrders();
+
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(orders, "Orders fetched successfully"));
     }
 
     @PostMapping
@@ -30,13 +38,16 @@ public class OrderController {
     }
 
     @DeleteMapping("/{id}")
-    public void deleteOrder(@PathVariable Long id){
-        throw new UnsupportedOperationException("Not implemented");
+    public ResponseEntity<ApiResponse<Void>> deleteOrder(@PathVariable Long id) {
+        orderService.deleteOrder(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(null, "Order deleted successfully"));
     }
 
     @GetMapping("/{id}")
-    public Order getOrderById(@PathVariable Long id){
-        throw new UnsupportedOperationException("Not implemented");
+    public ResponseEntity<ApiResponse<GetOrderResponseDto>> getOrderById(@PathVariable Long id){
+       GetOrderResponseDto order = orderService.getOrderById(id);
+       return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(order, "Orders fetched successfully"));
     }
 
     @GetMapping("/user/{userId}")
